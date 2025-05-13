9fb024844f606171161581ac483f4ecede9c6235
miles <719713496@qq.com>
2025-05-14 01:05:04 +0800

    add_pic

--- src/views/HomeComponent.vue
+++ src/views/HomeComponent.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="w-screen h-screen flex">
-    <!-- <div class="w-[calc(50%)] h-full" ref="contentRef" id="shareArea">
-      <CesiumContainer />
-    </div> -->
-    <div class="w-full h-full">
-      <TestShare />
+    <div
+      id="shareArea"
+      ref="contentRef"
+      class="w-[calc(50%)] h-[100px] overflow-y-auto markBorderR"
+    >
+      <!-- <CesiumContainer /> -->
+      <p>这是一段文本1</p>
+      <p>这是一段文本2</p>
+      <p>这是一段文本3</p>
+      <p>这是一段文本4</p>
+      <p>这是一段文本5</p>
+      <p>这是一段文本6</p>
+      <p>这是一段文本7</p>
+      <p>这是一段文本89</p>
+    </div>
+    <div class="w-[calc(50%)] h-full">
+      <PicShare />
     </div>
   </div>
 </template>
@@ -13,5 +25,6 @@
 import { ref } from 'vue'
 import CesiumContainer from './CesiumContainer/index.vue'
 import TestShare from './StreamDom/TestShare.vue'
+import PicShare from './StreamDom/PicShare.vue'
 const contentRef = ref<HTMLDivElement | null>(null)
 </script>
